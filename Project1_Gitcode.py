24cc4f46f83540923cfd845e633b8ae3638f5656
Florence Sergile-Etienne <113218128+fsergile-etienne@users.noreply.github.com>
2023-09-04 13:23:14 -0400

    Add files via upload

---
+++ Project1_Gitcode.py
@@ -0,0 +1,7 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Creating a project for GitHub
+>>> #By Florence Sergile-Etienne
+>>> 
+>>> print("COP2002 Fall 2023")
+COP2002 Fall 2023
